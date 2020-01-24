7693a73422ba6112eb8eb875e78e459ba05397f4
Ahmed Bilal <ahmed.bilal@netrevelation.com>
2020-01-24 12:14:34 +0500

    First commit

---
+++ Explicit.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace No_Such_Element_Exception_Handle
+{
+
+    public static class Explicit
+
+    {
+
+        public static IWebDriver driver;
+
+        public static void Wait (IWebDriver driver, By method)
+        {
+
+
+            try
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5)); // The default time out limit must be defined
+                IWebElement abc = wait.Until(ExpectedConditions.ElementIsVisible(method));
+
+                if (abc.Displayed)
+
+                {
+                    System.Console.WriteLine("The element found");
+
+                }
+
+            }
+
+            catch (Exception e) // Insome case I have Nosuchelement Exception but in this case TimeOutException Throws by system,
+            // code does not handle that exception , therefore I have set Catch block to handle any exception and its working.
+
+            {
+                System.Console.WriteLine("The element not found");
+
+            }
+
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+
+namespace No_Such_Element_Exception_Handle
+{
+   
+    class Entrypoint
+    {
+        static void Main()
+        {
+
+            IWebDriver driver = new ChromeDriver("./");
+            driver.Navigate().GoToUrl("https://demo.nopcommerce.com/");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            Thread.Sleep(5000);
+
+            IWebElement Reglink = driver.FindElement(By.ClassName("ico-register"));
+            Reglink.Click();
+
+            Explicit.Wait(driver, (By.Id("Email"))); // Calling Centralise method of Other Class  It pass the driver and relevant find element method as parameter.           
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
+
+//References
+// https://stackoverflow.com/questions/6992993/selenium-c-sharp-webdriver-wait-until-element-is-present
+//https://sqa.stackexchange.com/questions/15327/explicit-wait-is-not-reliable-with-selenium-c
